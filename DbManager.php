0893fc907f55c5f9aa7847c22d087763e49a77bc
IkomaMasahiro <14b1233@andrew.ac.jp>
2017-01-17 18:06:03 +0900

    Add files via upload

---
+++ DbManager.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 将寛
+ * Date: 2017/01/14
+ * Time: 17:20
+ */
+function getDb() {
+    $dsn = 'mysql:dbname=keijibandb; host=127.0.0.1';
+    $usr = 'root';
+    $passwd = '';
+
+    try {
+        $db = new PDO($dsn, $usr, $passwd);
+        $db->exec('SET NAMES utf8');
+        echo '接続に成功しました';
+        $db = NULL;
+    } catch (PDOException $e) {
+        die("接続エラー:{$e->getMessage()}");
+    }
+    return $db;
+}
