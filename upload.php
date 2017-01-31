08174037b0f4b9c48d241d349a40139de7756e4e
IkomaMasahiro <14b1233@andrew.ac.jp>
2017-01-31 17:17:22 +0900

    Add files via upload

--- upload.php
+++ upload.php
@@ -13,13 +13,12 @@
 </head>
 <body>
 <form method="POST" action="upload.php">
-    <div align="center">
-        ユーザID:
-        <input type="text" name="id" size="25" maxlength="10"/><br/>
-        ユーザネーム:
-        <input type="text" name="name" size="25" maxlength="30" /><br><br>
+    <div align="center"><br><br>
 
-        本文<br><br>
+        ユーザネーム:
+        <input type="text" name="name" size="25" maxlength="30" /><br/><br/>
+
+        本文<br>
         <textarea name = "contents" rows = "10" cols = "100" maxlength = "200"></textarea><br><br>
 
     <input type="submit" name="bottom" value="投稿"/>
@@ -34,27 +33,33 @@ require_once 'DbManager.php';
 require_once 'encodek1.php';
 $db = getDb();
 
-if (!isset($_POST["id"],$_POST["name"],$_POST["contents"])){             //未入力の処理
-    return;}
+if (isset($_POST["name"],$_POST["contents"])) {
 
+    if ($_POST['name'] === '' || $_POST["contents"] === '') {          //未入力の表示
+        echo '<span style="color: #fa0000">【注意】　すべての項目を入力してください</span>';
+        return;
+    }
 
-if ($_POST['id']===''||$_POST['name']===''||$_POST["contents"]===''){          //未入力の表示
-    echo '<span style="color: #fa0000">【注意】　すべての項目を入力してください</span>';
-    return;}
+    if ($_POST['name']>30){ //文字数制限の処理
+        echo '<span style="color: #fa0000">文字数は30字までです</span>';
+        return;
 
-
-try {
-    $db = getDb();
+    }elseif ($_POST['contents']>200){
+        echo '<span style="color: #fa0000">文字数は200文字までです</span>';
+        return;
+    }
+    try {
+        $db = getDb();
 // INSERT命令の準備
-    $stt = $db->prepare('INSERT INTO POST(ID,NAME,CONTENTS) VALUES(:id,:name,:contents)');
+        $stt = $db->prepare('INSERT INTO post3(name,contents) VALUES(:name,:contents)');
 //INSERT命令を実行
-    $stt->bindValue(':id', $_POST['id']);//このあたりの復習を行う
-    $stt->bindValue(':name',$_POST['name']);
-    $stt->bindValue(':contents',$_POST['contents']);
-    $stt->execute();
-    $db = NULL;
-}catch(PDOException $e) {
-    die("エラーメッセージ:{$e->getMessage()}");
+        $stt->bindValue(':name', $_POST['name']);
+        $stt->bindValue(':contents', $_POST['contents']);
+        $stt->execute();
+        $db = NULL;
+    } catch (PDOException $e) {
+        die("エラーメッセージ:{$e->getMessage()}");
+    }
 }
 ?>
     <div align="center">投稿表示
@@ -67,14 +72,14 @@ try {
 <?php
 try {
     $db = getDb();
-    $stt = $db->prepare('SELECT * FROM post ORDER BY id DESC ');
+    $stt = $db->prepare('SELECT * FROM post3 ORDER BY id DESC ');
     $stt->execute();
     while ($row = $stt->fetch(PDO::FETCH_ASSOC)) {
         ?>
         <tr>
-            <td><?php print $row['id']; ?></td>
-            <td><?php e($row['name']); ?></td>
-            <td><?php print $row['contents']; ?></td>
+            <td><?php print($row['id']); ?></td>
+            <td><?php print($row['name']); ?></td>
+            <td><?php print($row['contents']); ?></td>
         </tr>
         <?php
     }
